f8ad0484ebd26b168f9b8807ed40d64befb44c89
Serena Silver <39900622+serena-silver@users.noreply.github.com>
2018-07-23 10:09:02 +0200

    Add files via upload

---
+++ a_constant/scripts/script.js
@@ -0,0 +1,877 @@
+
+
+
+// video 2
+
+
+$( document ).ready(function() {
+
+
+	$('#humiliation').click(function(){
+		$( '.films' ).animate({ zIndex: 100,}, 1 );
+		$( '#video3' ).animate({opacity: 100, zIndex:100,}, 1000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films' ).animate({ zIndex: -100,}, 1 );
+		$( '#video3' ).animate({opacity: 0, zIndex:-100,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+
+		
+
+	});
+
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#rush').click(function(){
+		$( '.films2' ).animate({ zIndex: 100,}, 1 );
+		$( '#video2' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films2' ).animate({ zIndex: -10,}, 1 );
+		$( '#video2' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#lack').click(function(){
+		$( '.films1' ).animate({ zIndex: 100,}, 1 );
+		$( '#video1' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films1' ).animate({ zIndex: -10,}, 1 );
+		$( '#video1' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+window.addEventListener ("DOMContentLoaded",handleWindowLoad);
+function handleWindowLoad ()
+{
+    var Video = document.getElementById ( "Example_Video" );
+    var PlayButton = document.getElementById ( "Play_Pause" );
+    var MuteButton = document.getElementById ( "Mute_Unmute" );
+    var Slider = document.getElementById ( "Slider" );
+
+    PlayButton.addEventListener ( "click", Play_Pause_Video ) ;
+
+    function Play_Pause_Video ()
+    {
+        if (Video.paused === true)
+        {
+            Video.play();
+            PlayButton.innerHTML = "Pause";
+        }
+        else
+        {
+            Video.pause();
+            PlayButton.innerHTML = "Play";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#spurt').click(function(){
+		$( '.films4' ).animate({ zIndex: 100,}, 1 );
+		$( '#video4' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films4' ).animate({ zIndex: -10,}, 1 );
+		$( '#video4' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#impotence').click(function(){
+		$( '.films5' ).animate({ zIndex: 100,}, 1 );
+		$( '#video5' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films5' ).animate({ zIndex: -10,}, 1 );
+		$( '#video5' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#outbreak').click(function(){
+		$( '.films6' ).animate({ zIndex: 100,}, 1 );
+		$( '#video6' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films6' ).animate({ zIndex: -10,}, 1 );
+		$( '#video6' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#slump').click(function(){
+		$( '.films7' ).animate({ zIndex: 100,}, 1 );
+		$( '#video7' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films7' ).animate({ zIndex: -10,}, 1 );
+		$( '#video7' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#blast').click(function(){
+		$( '.films9' ).animate({ zIndex: 100,}, 1 );
+		$( '#video9' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films9' ).animate({ zIndex: -10,}, 1 );
+		$( '#video9' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#ridge').click(function(){
+		$( '.films8' ).animate({ zIndex: 100,}, 1 );
+		$( '#video8' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films8' ).animate({ zIndex: -10,}, 1 );
+		$( '#video8' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#crush').click(function(){
+		$( '.films10' ).animate({ zIndex: 100,}, 1 );
+		$( '#video10' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films10' ).animate({ zIndex: -10,}, 1 );
+		$( '#video10' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#irritation').click(function(){
+		$( '.films11' ).animate({ zIndex: 100,}, 1 );
+		$( '#video11' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films11' ).animate({ zIndex: -10,}, 1 );
+		$( '#video11' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#puff').click(function(){
+		$( '.films12' ).animate({ zIndex: 100,}, 1 );
+		$( '#video12' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films12' ).animate({ zIndex: -10,}, 1 );
+		$( '#video12' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#drag').click(function(){
+		$( '.films13' ).animate({ zIndex: 100,}, 1 );
+		$( '#video13' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films13' ).animate({ zIndex: -10,}, 1 );
+		$( '#video13' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#jaded').click(function(){
+		$( '.films14' ).animate({ zIndex: 100,}, 1 );
+		$( '#video14' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films14' ).animate({ zIndex: -10,}, 1 );
+		$( '#video14' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+$( document ).ready(function() {
+
+
+	$('#draft').click(function(){
+		$( '.films15' ).animate({ zIndex: 100,}, 1 );
+		$( '#video15' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films15' ).animate({ zIndex: -10,}, 1 );
+		$( '#video15' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#twitch').click(function(){
+		$( '.films16' ).animate({ zIndex: 100,}, 1 );
+		$( '#video16' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films16' ).animate({ zIndex: -10,}, 1 );
+		$( '#video16' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#crunch').click(function(){
+		$( '.films17' ).animate({ zIndex: 100,}, 1 );
+		$( '#video17' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films17' ).animate({ zIndex: -10,}, 1 );
+		$( '#video17' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#woe').click(function(){
+		$( '.films18' ).animate({ zIndex: 100,}, 1 );
+		$( '#video18' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films18' ).animate({ zIndex: -10,}, 1 );
+		$( '#video18' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#blue').click(function(){
+		$( '.films19' ).animate({ zIndex: 100,}, 1 );
+		$( '#video19' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films19' ).animate({ zIndex: -10,}, 1 );
+		$( '#video19' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#slumber').click(function(){
+		$( '.films20' ).animate({ zIndex: 100,}, 1 );
+		$( '#video20' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films20' ).animate({ zIndex: -10,}, 1 );
+		$( '#video20' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#black').click(function(){
+		$( '.films21' ).animate({ zIndex: 100,}, 1 );
+		$( '#video21' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films21' ).animate({ zIndex: -10,}, 1 );
+		$( '#video21' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#in').click(function(){
+		$( '.films22' ).animate({ zIndex: 100,}, 1 );
+		$( '#video22' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films22' ).animate({ zIndex: -10,}, 1 );
+		$( '#video22' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#light').click(function(){
+		$( '.films23' ).animate({ zIndex: 100,}, 1 );
+		$( '#video23' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films23' ).animate({ zIndex: -10,}, 1 );
+		$( '#video23' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#dis').click(function(){
+		$( '.films24' ).animate({ zIndex: 100,}, 1 );
+		$( '#video24' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+});
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films24' ).animate({ zIndex: -10,}, 1 );
+		$( '#video24' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+$( document ).ready(function() {
+
+
+	$('#shame').click(function(){
+		$( '.films25' ).animate({ zIndex: 100,}, 1 );
+		$( '#video25' ).animate({opacity: 100, zIndex:100,}, 2000 );
+		$( '.x').animate({opacity: 100, zIndex:105,}, 1 );
+		
+
+	});
+
+
+});
+
+
+
+$( document ).ready(function() {
+
+
+	$('.x').click(function(){
+		$( '.films25' ).animate({ zIndex: -10,}, 1 );
+		$( '#video25' ).animate({opacity: 0, zIndex:-10,}, 1 );
+		$( '.x').animate({opacity: 0, zIndex:0,}, 1 );
+		$('video').each(function () { this.pause() });
+		
+
+	});
+
+});
+
+
+
+
+
+
